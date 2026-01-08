67e3e6e67041c51facccc73ca6034966ef526bd9
brms2021 <86517410+brms2021@users.noreply.github.com>
2026-01-08 15:09:51 +1100

    first commit

--- api/src/Controllers/LLMTaskController/llm-task.controller.ts
+++ api/src/Controllers/LLMTaskController/llm-task.controller.ts
@@ -1,49 +1,9 @@
-import { ValidationPipe, UsePipes, Controller, Get, Headers, Param, Post, Body, Put, Delete, Res } from '@nestjs/common';
-// import { CreateCatDto } from './dto/create-cat.dto';
-// import { CatsService } from './cats.service';
+import { Controller } from '@nestjs/common';
 import { LLMTaskService } from './llm-task.service'
-// import { Response as Res } from 'express';
-import { API_ROUTES, User } from '../../constants/constants';
-import { RolesGuard } from '../../auth/roles.guard';
-import { Roles } from '../../auth/roles.decorator';
-import { UseGuards } from '@nestjs/common';
-import { LE_USER_ROLES } from 'src/constants/constants'
-
-import {
-  ConversationTopicDto,
-  ConversationTopicRequestDto,
-  ConversationSampleDto
-} from './llm-task.dto'
-
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
-import { AxiosResponse } from 'axios';
+import { API_ROUTES } from '../../constants/constants';
 
 @Controller(API_ROUTES.LLM_TASK())
 export class LLMTaskController {
   constructor(private service: LLMTaskService) {}
-
-  @Post('/:accountId/generate_conversations')
-  @ApiOperation({ summary: 'Add Topic' })
-  @ApiResponse({ status: 200, description: 'The topic has been successfully added.'})
-  @ApiResponse({ status: 201, description: 'The topic has been successfully added.'})
-  @ApiResponse({ status: 401, description: 'Unauthorized.' })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
-  @UsePipes(new ValidationPipe({ transform: false, whitelist: true}))
-  @UseGuards(RolesGuard)
-  @Roles([LE_USER_ROLES.ADMIN])
-  async createUser(
-    @Headers('authorization') token: string,
-    @User() user: any,
-    @Body() body: any
-  ): Promise<any> {
-    const responseBody = await this.service.generateConversations(body)
-    console.info(responseBody)
-    return responseBody
-  }
   
 }
--- api/src/Controllers/LLMTaskController/llm-task.service.ts
+++ api/src/Controllers/LLMTaskController/llm-task.service.ts
@@ -1,28 +1,11 @@
 import {
   Injectable,
-  Inject,
-  Logger,
-  InternalServerErrorException,
 } from '@nestjs/common';
-import OpenAI from 'openai';
 import OpenAIApi from 'openai';
-import { CollectionReference } from '@google-cloud/firestore';
-import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom } from 'rxjs';
-import { AxiosResponse, AxiosError } from 'axios';
-import { helper } from 'src/utils/HelperService';
-import { uuid } from 'short-uuid';
-import { IUser } from 'src/interfaces/interfaces'
-import { defaultPrompts } from './llm-task.constants'
-import {
-  ConversationTopicDto,
-  ConversationTopicRequestDto,
-  ConversationSampleDto
-} from './llm-task.dto'
 
 @Injectable()
 export class LLMTaskService {  
-  private logger: Logger = new Logger(LLMTaskService.name);
+  // private logger: Logger = new Logger(LLMTaskService.name);
   public openai: OpenAIApi;
 
   constructor(
